2a747356b43ac980729815e77bfe98898c32a749
sawanoboly <sawanoboriyu@higanworks.com>
2016-05-10 15:28:28 +0900

    more detail for password description

--- params/rds_settings.rb
+++ params/rds_settings.rb
@@ -1,5 +1,5 @@
 MySQLPassword do
-  Description "Password of RDS User"
+  Description "Password of RDS User. It must be included at least one or more upper-case, lower-case and numeric character. Special charactors are not allowed."
   Type        "String"
   MinLength   8
   MaxLength   64
