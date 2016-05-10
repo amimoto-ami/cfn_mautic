82d506771a9c1c1e48334a0efa0b43a5c348e8fc
sawanoboly <sawanoboriyu@higanworks.com>
2016-05-10 13:20:47 +0900

    update amis to 1.3.1-2

--- mautic/ami_hvm.rb
+++ mautic/ami_hvm.rb
@@ -1,15 +1,15 @@
 ## "Mautic 1.3.0-1*"
 MPAMIs(
   {
-    "eu-west-1"=>{"AMI"=>"ami-386dd34b"},
-    "ap-southeast-1"=>{"AMI"=>"ami-ecc8008f"},
-    "ap-southeast-2"=>{"AMI"=>"ami-c36849a0"},
-    "eu-central-1"=>{"AMI"=>"ami-186b8e77"},
-    "ap-northeast-2"=>{"AMI"=>"ami-d75d93b9"},
-    "ap-northeast-1"=>{"AMI"=>"ami-e048448e"},
-    "us-east-1"=>{"AMI"=>"ami-6d310807"},
-    "sa-east-1"=>{"AMI"=>"ami-7c028010"},
-    "us-west-1"=>{"AMI"=>"ami-43e59523"},
-    "us-west-2"=>{"AMI"=>"ami-e8f91588"},
+    "eu-west-1"=>{"AMI"=>"ami-2d3cbf5e"},
+    "ap-southeast-1"=>{"AMI"=>"ami-ec26f38f"},
+    "ap-southeast-2"=>{"AMI"=>"ami-e50e2c86"},
+    "eu-central-1"=>{"AMI"=>"ami-17947478"},
+    "ap-northeast-2"=>{"AMI"=>"ami-cdc50da3"},
+    "ap-northeast-1"=>{"AMI"=>"ami-da2032b4"},
+    "us-east-1"=>{"AMI"=>"ami-b95a49d3"},
+    "sa-east-1"=>{"AMI"=>"ami-54d05e38"},
+    "us-west-1"=>{"AMI"=>"ami-03dea263"},
+    "us-west-2"=>{"AMI"=>"ami-24ab5c44"},
   }
 )
